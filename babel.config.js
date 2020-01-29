794f1286affeb6739f977e94b9b6cc3e9c01d4c2
Marie Slavina <morefly@yandex.ru>
2020-01-29 16:44:17 +0300

    webpack-config

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,14 @@ const presets = [
   [
     '@babel/env',
     {
-      // TODO add chrome mobile, yandex
       targets: {
+        android: '67',
         edge: '15',
-        firefox: '50',
+        firefox: '60',
         chrome: '64',
         safari: '11.1',
+        ios: '12',
+        esmodules: true,
       },
       useBuiltIns: 'usage',
       corejs: '3.4.1',
