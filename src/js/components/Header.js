cca1dcbfed20fed8a1d5be233599df0130beb7f6
Marie Slavina <morefly@yandex.ru>
2020-03-04 18:06:47 +0300

    update constructor in Header

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,37 +2,36 @@ export default class Header {
   constructor(domElement) {
     this.domElement = domElement;
     this.render = this.render.bind(this);
+    this.savedMessages = this.domElement
+      .querySelector('.header__saved-massages');
+    this.logout = this.domElement
+      .querySelector('.header__logout');
+    this.userName = this.domElement
+      .querySelector('.header__user-name');
+    this.login = this.domElement
+      .querySelector('.header__login');
   }
 
   // рендерит шапку
   render(isLoggedIn, userName) {
     if (isLoggedIn) {
-      this.domElement
-        .querySelector('.header__saved-massages')
+      this.savedMessages
         .classList.add('header__saved-massages_is-open');
-      this.domElement
-        .querySelector('.header__logout')
+      this.logout
         .classList.add('header__logout_is-opened');
-      this.domElement
-        .querySelector('.header__user-name')
+      this.userName
         .textContent = userName;
-      this.domElement
-        .querySelector('.header__login')
+      this.login
         .classList.remove('header__login_is-opened');
     } else {
-      this.domElement
-        .querySelector('.header__saved-massages')
+      this.savedMessages
         .classList.remove('header__saved-massages_is-open');
-      this.domElement
-        .querySelector('.header__logout')
+      this.logout
         .classList.remove('header__logout_is-opened');
-      this.domElement
-        .querySelector('.header__user-name')
+      this.userName
         .textContent = '';
-      this.domElement
-        .querySelector('.header__login')
+      this.login
         .classList.add('header__login_is-opened');
     }
   }
 }
-
